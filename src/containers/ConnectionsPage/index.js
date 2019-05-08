8cb49be41ef4c875bde84c797f832887e533c045
geecko <H.guillaume@gmail.com>
2019-05-08 17:40:12 +0200

    Feat(ConnectionsPage): add datetimepicker and text

--- src/containers/ConnectionsPage/index.js
+++ src/containers/ConnectionsPage/index.js
@@ -2,26 +2,58 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import propTypes from 'prop-types';
 import axios from 'axios';
+import queryString from 'query-string';
+import Datetime from 'react-datetime';
+
 import s from './style.module.scss';
 
 class ConnectionsPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      graph: [],
-      style: {
-        'border-style': 'solid'
-      }
+      targetUrl: url,
+      timestamp: departureTime,
+      graph: []
     };
+    this.datePicker = React.createRef();
+    this.getNextData = this.getNextData.bind(this);
+    this.getPreviousData = this.getPreviousData.bind(this);
+    this.fetchData = this.fetchData.bind(this);
+    this.onDateSelected = this.onDateSelected.bind(this);
   }
 
   componentDidMount() {
-    axios.get(`https://graph.irail.be/sncb/connections?departureTime=2019-05-06T14:39:00.000Z`)
+    this.fetchData(this.state.targetUrl);
+  }
+
+  getPreviousData() {
+    this.fetchData(this.state.previousPage);
+  }
+
+  getNextData() {
+    this.fetchData(this.state.nextPage);
+  }
+
+  fetchData(targetUrl) {
+    const timestamp = targetUrl.split("?departureTime=")[1];
+
+    this.setState({
+      targetUrl: targetUrl,
+      timestamp: timestamp,
+      graph: []
+    });
+
+    axios.get(targetUrl)
       .then(res => {
-        const nextPage = res.data["hydra:next"];
-        const previousPage = res.data["hydra:previous"];
-        this.setState({ graph: res.data["@graph"] });
+        const newURL = "?departureTime=" + timestamp;
+        window.history.pushState({}, null, newURL);
+        this.setState({ graph: res.data["@graph"], nextPage: res.data["hydra:next"], previousPage: res.data["hydra:previous"] });
       });
+  }
+
+  onDateSelected(date) {
+    const url = "https://graph.irail.be/sncb/connections?departureTime=" + date.toISOString();
+    this.fetchData(url);
   }
 
   render() {
@@ -29,34 +61,57 @@ class ConnectionsPage extends React.Component {
     const items = []
 
     for (const [index, connection] of graph.entries()) {
-      items.push(<tr style={style} row={index}>
-          <td style={style}><a href={connection["@id"]}>{connection["@id"].split("/").pop()}</a></td>
-          <td style={style}>{connection["direction"]}</td>
-          <td style={style}><a href={connection["departureStop"]}>{connection["departureStop"].split("/").pop()}</a></td>
-          <td style={style}><a href={connection["arrivalStop"]}>{connection["arrivalStop"].split("/").pop()}</a></td>
-          <td style={style}>{connection["departureTime"].split(".")[0].replace("T", "\n")}</td>
-          <td style={style}>{connection["arrivalTime"].split(".")[0].replace("T", "\n")}</td>
-          <td style={style}><a href={connection["gtfs:trip"]}>Trip</a> <a href={connection["gtfs:route"]}>Route</a></td>
-          <td style={style}>{connection["departureDelay"] || "0"}</td>
-          <td style={style}>{connection["arrivalDelay"] || "0"}</td>
+      items.push(<tr row={index}>
+          <td><a href={connection["@id"]}>{connection["@id"].split("/").pop()}</a></td>
+          <td>{connection["direction"]}</td>
+          <td><a href={connection["departureStop"]}>{connection["departureStop"].split("/").pop()}</a></td>
+          <td><a href={connection["arrivalStop"]}>{connection["arrivalStop"].split("/").pop()}</a></td>
+          <td>{new Date(connection["departureTime"]).toString()}</td>
+          <td>{new Date(connection["arrivalTime"]).toString()}</td>
+          <td><a href={connection["gtfs:trip"]}>Trip</a><br/><a href={connection["gtfs:route"]}>Route</a></td>
+          <td>{connection["departureDelay"] || "0"}</td>
+          <td>{connection["arrivalDelay"] || "0"}</td>
         </tr>)
     }
 
     return (
-      <table>
-        <tr>
-          <th>Connection ID</th>
-          <th>Direction</th>
-          <th>Departure Stop</th>
-          <th>Arrival Stop</th>
-          <th>Departure Time</th>
-          <th>Arrival Time</th>
-          <th>GTFS</th>
-          <th>Departure Delay</th>
-          <th>Arrival Delay</th>
-        </tr>
-        {items}
-      </table>
+      <div>
+        <title>Linked Connections | Sandbox</title>
+        <h1>Quick showcase</h1>
+        <p>This page loads all the data for the connections departing at a given time and displays it all in a table.</p>
+        <h3>See the API request</h3>
+        <div id="urlDiv" className={s.inputDiv}>
+          <div className={s.inputDiv__key}>URL</div>
+          <div className={s.requestUrl}>
+            <a href={this.state.targetUrl}>
+              <span className={s.api}>https://graph.irail.be</span>
+              <span className={s.path}>/sncb</span>
+              <span className={s.feature}>/connections</span>
+              <span className={s.parameters}>{"?departureTime=" + this.state.timestamp}</span>
+            </a>
+          </div>
+        </div>
+        <p>Selected date: <b>{new Date(this.state.timestamp).toString()}</b></p>
+        <Datetime className={s.datePicker} ref={this.datePicker} input={false} defaultValue={new Date(this.state.timestamp)} onChange={this.onDateSelected}/>
+        <button type="button" onClick={this.getPreviousData}>←</button>
+        <button type="button" onClick={this.getNextData}>→</button>
+        <table>
+          <tbody>
+            <tr>
+              <th>Connection ID</th>
+              <th>Direction</th>
+              <th>Departure Stop</th>
+              <th>Arrival Stop</th>
+              <th>Departure Time</th>
+              <th>Arrival Time</th>
+              <th>GTFS</th>
+              <th>Departure Delay</th>
+              <th>Arrival Delay</th>
+            </tr>
+            {items}
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
