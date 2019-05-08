35171e0d9124a6cbed537845f769b5731e43d8f6
geecko <H.guillaume@gmail.com>
2019-05-08 04:59:18 +0200

    feat(ConnectionsPage): use URL parameter

--- src/containers/ConnectionsPage/index.js
+++ src/containers/ConnectionsPage/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import propTypes from 'prop-types';
 import axios from 'axios';
-import s from './style.module.scss';
+import queryString from 'query-string';
+import './style.module.scss';
 
 class ConnectionsPage extends React.Component {
   constructor(props) {
     super(props);
+    const value = queryString.parse(props.location.search);
+    const departureTime = value.token || "2019-05-06T14:39:00.000Z";
+    const url = "https://graph.irail.be/sncb/connections?departureTime=" + departureTime;
     this.state = {
-      targetUrl: 'https://graph.irail.be/sncb/connections?departureTime=2019-05-06T14:39:00.000Z',
-      graph: [],
-      style: {
-        'border-style': 'solid'
-      }
+      targetUrl: url,
+      graph: []
     };
     this.getNextData = this.getNextData.bind(this);
     this.getPreviousData = this.getPreviousData.bind(this);
@@ -42,25 +41,27 @@ class ConnectionsPage extends React.Component {
   fetchData() {
     axios.get(this.state.targetUrl)
       .then(res => {
+        const newURL = "?departureTime=" + this.state.targetUrl.split("?departureTime=")[1];
+        window.history.pushState({}, null, newURL);
         this.setState({ graph: res.data["@graph"], nextpage: res.data["hydra:next"], previousPage: res.data["hydra:previous"] });
       });
   }
 
   render() {
-    const { graph, style } = this.state;
+    const { graph } = this.state;
     const items = []
 
     for (const [index, connection] of graph.entries()) {
-      items.push(<tr style={style} row={index}>
-          <td style={style}><a href={connection["@id"]}>{connection["@id"].split("/").pop()}</a></td>
-          <td style={style}>{connection["direction"]}</td>
-          <td style={style}><a href={connection["departureStop"]}>{connection["departureStop"].split("/").pop()}</a></td>
-          <td style={style}><a href={connection["arrivalStop"]}>{connection["arrivalStop"].split("/").pop()}</a></td>
-          <td style={style}>{connection["departureTime"].split(".")[0].replace("T", "\n")}</td>
-          <td style={style}>{connection["arrivalTime"].split(".")[0].replace("T", "\n")}</td>
-          <td style={style}><a href={connection["gtfs:trip"]}>Trip</a> <a href={connection["gtfs:route"]}>Route</a></td>
-          <td style={style}>{connection["departureDelay"] || "0"}</td>
-          <td style={style}>{connection["arrivalDelay"] || "0"}</td>
+      items.push(<tr row={index}>
+          <td><a href={connection["@id"]}>{connection["@id"].split("/").pop()}</a></td>
+          <td>{connection["direction"]}</td>
+          <td><a href={connection["departureStop"]}>{connection["departureStop"].split("/").pop()}</a></td>
+          <td><a href={connection["arrivalStop"]}>{connection["arrivalStop"].split("/").pop()}</a></td>
+          <td>{connection["departureTime"].split(".")[0].replace("T", "\n")}</td>
+          <td>{connection["arrivalTime"].split(".")[0].replace("T", "\n")}</td>
+          <td><a href={connection["gtfs:trip"]}>Trip</a><br/><a href={connection["gtfs:route"]}>Route</a></td>
+          <td>{connection["departureDelay"] || "0"}</td>
+          <td>{connection["arrivalDelay"] || "0"}</td>
         </tr>)
     }
 
@@ -70,18 +71,20 @@ class ConnectionsPage extends React.Component {
         <button type="button" onClick={this.getPreviousData}>←</button>
         <button type="button" onClick={this.getNextData}>→</button>
         <table>
-          <tr>
-            <th>Connection ID</th>
-            <th>Direction</th>
-            <th>Departure Stop</th>
-            <th>Arrival Stop</th>
-            <th>Departure Time</th>
-            <th>Arrival Time</th>
-            <th>GTFS</th>
-            <th>Departure Delay</th>
-            <th>Arrival Delay</th>
-          </tr>
-          {items}
+          <tbody>
+            <tr>
+              <th>Connection ID</th>
+              <th>Direction</th>
+              <th>Departure Stop</th>
+              <th>Arrival Stop</th>
+              <th>Departure Time</th>
+              <th>Arrival Time</th>
+              <th>GTFS</th>
+              <th>Departure Delay</th>
+              <th>Arrival Delay</th>
+            </tr>
+            {items}
+          </tbody>
         </table>
       </div>
     )
